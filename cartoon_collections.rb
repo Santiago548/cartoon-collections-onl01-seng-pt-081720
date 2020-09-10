195064ee24e1cf4a7dc000883d48a114f0bf765d
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-10 20:53:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,10 @@ def long_planeteer_calls(planeteer_calls)
     planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(cheese)
-  cheese.find{|cheese| cheese.is_a?(String)}
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
