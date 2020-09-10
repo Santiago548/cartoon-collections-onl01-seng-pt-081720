e1a8d0549ce34825689d52f506f296132f7d475a
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-10 20:41:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,12 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |call| call.capitalize << "!"
-
   end
+end
 
+def long_planeteer_calls(planeteer_calls)
+  if planeteer_calls.length > 4
+    puts true
+  else
+    false
 end
